6a50dc7aaeb76818422ac120fdb737041eed4752
Angelos Barmpoutis <angelosbar97@gmail.com>
2022-07-04 22:22:39 +0300

    Reduce hero top padding

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,7 +1,7 @@
 const HeroImage = (props) => {
     if (props.posterPath) {
         return (
-            <div className={`hero__images__image ${props.active}`} style={{background:`linear-gradient(30deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2)), url(https://image.tmdb.org/t/p/w780${props.posterPath}) center center/cover no-repeat`}}>
+            <div className={`hero__images__image ${props.active}`} style={{background:`linear-gradient(30deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.2)), url(https://image.tmdb.org/t/p/w780${props.posterPath}) center center/cover no-repeat`}}>
                 <h2>{props.title}</h2>
             </div>
         )
