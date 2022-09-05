df3e7019185b86f5254ca87f6f9e1836ed35a63c
Angelos Barmpoutis <angelosbar97@gmail.com>
2022-09-05 23:28:24 +0300

    Fix the bug that caused Hero not to open the modal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ const Footer = () => {
         <footer>
             <div className="wrapper">
                 <p>Designed & created by <a href="https://github.com/Angelos-Barmpoutis" target='_blank' rel="noreferrer">Angelos Barmpoutis</a></p>
-                <p>Copyright &copy; 2022. All rights reserved.</p>
             </div>
         </footer>
     )
