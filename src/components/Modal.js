5e4fca5077c1eeadec93442c17b8bde102f8d99a
Angelos Barmpoutis <angelosbar97@gmail.com>
2022-09-24 15:15:01 +0300

    Redesign modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,29 @@
 import { motion } from "framer-motion";
+import styled from 'styled-components';
+import {Language} from '@styled-icons/ionicons-solid';
+import {Status} from '@styled-icons/fluentui-system-filled';
+import {ShowChart} from '@styled-icons/material-rounded';
+import {Slideshow} from '@styled-icons/boxicons-regular';
+import {Movie} from '@styled-icons/boxicons-regular';
+import {Category} from '@styled-icons/boxicons-solid';
+
+const StatusIcon = styled(Status)`
+background-color: rgb(56, 148, 60);`
+
+const RatingIcon = styled(ShowChart)`
+background-color: rgb(252, 146, 54);`
+
+const EpisodesIcon = styled(Slideshow)`
+background-color: rgb(105, 42, 252);`
+
+const SeasonsIcon = styled(Movie)`
+background-color: rgb(86, 19, 244);`
+
+const LanguageIcon = styled(Language)`
+background-color: rgb(40, 99, 248);`
+
+const TypeIcon = styled(Category)`
+background-color: rgb(189, 41, 66);`
 
 const Modal = ({modal, closeModal}) => {
     // Map through genres' list and return a list of genres' names
@@ -64,8 +89,61 @@ const Modal = ({modal, closeModal}) => {
                             </div>
                         </div>
 
-                        <div className="modal__details__genres">
-                            {modal.results.genres && genresNames}
+                        <div className="modal__details__stats">
+                            {modal.results.type
+                                && <div className="modal__details__stats__item">
+                                        <TypeIcon />
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Type</h3>
+                                            <p>{modal.results.type}</p>
+                                        </div>
+                            </div>}
+
+                            {modal.results.status
+                                && <div className="modal__details__stats__item">
+                                        <StatusIcon/>
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Status</h3>
+                                            <p>{modal.results.status}</p>
+                                        </div>
+                            </div>}
+
+
+                            {modal.results.original_language
+                                && <div className="modal__details__stats__item">
+                                        <LanguageIcon />
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Original Language</h3>
+                                            <p>{modal.results.original_language}</p>
+                                        </div>
+                            </div>}
+
+                            {modal.results.number_of_seasons
+                                && <div className="modal__details__stats__item">
+                                        <SeasonsIcon />
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Seasons</h3>
+                                            <p>{modal.results.number_of_seasons}</p>
+                                        </div>
+                            </div>}
+                        
+                            {modal.results.number_of_episodes !== 0 && modal.results.number_of_episodes
+                                && <div className="modal__details__stats__item">
+                                        <EpisodesIcon />
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Episodes</h3>
+                                            <p>{modal.results.number_of_episodes}</p>
+                                        </div>
+                            </div>}
+
+                            {modal.results.vote_average !== 0
+                                && <div className="modal__details__stats__item">
+                                        <RatingIcon />
+                                        <div className="modal__details__stats__item__content">
+                                            <h3>Rating</h3>
+                                            <p>{modal.results.vote_average !== 0 && modal.results.vote_average !== 10 ? modal.results.vote_average.toFixed(1) : modal.results.vote_average}</p>
+                                        </div>
+                            </div>}
                         </div>
 
                         {modal.results.tagline
@@ -79,44 +157,9 @@ const Modal = ({modal, closeModal}) => {
                                     {modal.results.overview}
                                 </p>
                         </div>}
-                        
-                        <div className="modal__details__stats">
-                            {modal.results.type
-                                && <div className="modal__details__stats__item">
-                                        <h3>Type:</h3>
-                                        <p>{modal.results.type}</p>
-                            </div>}
 
-                            {modal.results.status
-                                && <div className="modal__details__stats__item">
-                                        <h3>Status:</h3>
-                                        <p>{modal.results.status}</p>
-                            </div>}
-
-
-                            {modal.results.original_language
-                                && <div className="modal__details__stats__item">
-                                        <h3>Original Language:</h3>
-                                        <p>{modal.results.original_language}</p>
-                            </div>}
-
-                            {modal.results.number_of_seasons
-                                && <div className="modal__details__stats__item">
-                                        <h3>Seasons:</h3>
-                                        <p>{modal.results.number_of_seasons}</p>
-                            </div>}
-                        
-                            {modal.results.number_of_episodes !== 0 && modal.results.number_of_episodes
-                                && <div className="modal__details__stats__item">
-                                        <h3>Episodes:</h3>
-                                        <p>{modal.results.number_of_episodes}</p>
-                            </div>}
-
-                            {modal.results.vote_average !== 0
-                                && <div className="modal__details__stats__item">
-                                        <h3>Rating:</h3>
-                                        <p>{modal.results.vote_average !== 0 && modal.results.vote_average !== 10 ? modal.results.vote_average.toFixed(1) : modal.results.vote_average} ({modal.results.vote_count})</p>
-                            </div>}
+                        <div className="modal__details__genres">
+                            {modal.results.genres && genresNames}
                         </div>
 
                     </section>
